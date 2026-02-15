8dc400c8241b526364359f3d91940e2144e69057
git-alexis <alex59.billemont@gmail.com>
2026-02-15 18:14:41 +0100

    Lancement de la fonction countPrimeNumbers() 100 fois

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+function isPrime(n) {
+	for (let i = 2; i * i < n; i++) {
+		if (n % i === 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function countPrimeNumbers() {
+	let count = 0;
+
+	for(let i = 2; i <= 100; i++) {
+		if(isPrime(i)) {
+			count += 1;
+		}
+	}
+
+	return count;
+}
+
+let start = performance.now();
+for(let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+let end = performance.now();
+let time = end - start;
+console.log("Execution time of calculating prime numbers 100 times was " + time + " milliseconds.");
